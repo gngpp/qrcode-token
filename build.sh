718fcf241d319d5592f2ed52de9d349203a92706
zf1976 <1596863112@qq.com>
2022-03-12 23:06:49 +0800

    test: test build

--- build.sh
+++ build.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 export GOPROXY=direct
 
-apt-get update
-apt-get install gcc-mingw-w64-i686 gcc-multilib upx-ucl
+sudo apt-get update
+sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_amd64_qrcode-token.tar.gz ./app
+tar -czvf linux_amd64_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_386_qrcode-token.tar.gz ./app
+tar -czvf linux_386_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=freebsd GOARCH=386 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf freebsd_386_qrcode-token.tar.gz ./app
+tar -czvf freebsd_386_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf freebsd_amd64_qrcode-token.tar.gz ./app
+tar -czvf freebsd_amd64_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=freebsd GOARCH=arm go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf freebsd_amd64_qrcode-token.tar.gz ./app
+tar -czvf freebsd_amd64_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_armv7_qrcode-token.tar.gz ./app
+tar -czvf linux_armv7_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_armv6_qrcode-token.tar.gz ./app
+tar -czvf linux_armv6_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_armv5_qrcode-token.tar.gz ./app
+tar -czvf linux_armv5_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_arm64_qrcode-token.tar.gz ./app
+tar -czvf linux_arm64_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=mips64 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_mips64_qrcode-token.tar.gz ./app
+tar -czvf linux_mips64_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=mips64le go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_mips64le_qrcode-token.tar.gz ./app
+tar -czvf linux_mips64le_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_mipsle_qrcode-token.tar.gz ./app
+tar -czvf linux_mipsle_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf linux_mips_qrcode-token.tar.gz ./app
+tar -czvf linux_mips_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf windows_386_qrcode-token.tar.gz ./app
+tar -czvf windows_386_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf windows_amd64_qrcode-token.tar.gz ./app
+tar -czvf windows_amd64_qrcode-token.tar.gz ./app
 
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o app ./main.go
-upx ./app && tar -czvf darwin_amd64_qrcode-token.tar.gz ./app
+tar -czvf darwin_amd64_qrcode-token.tar.gz ./app
